7ea80839ac68848a47f33a5d6afafdb782a70082
Metin Yeni <metinyeni33@gmail.com>
2023-09-27 00:24:38 +0300

    personel istatistik sayfası tamamlandı

--- is_takip_proje/Formlar/FrmPersonelIstatistik.cs
+++ is_takip_proje/Formlar/FrmPersonelIstatistik.cs
@@ -7,6 +7,7 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using is_takip_proje.Entity;
 
 namespace is_takip_proje.Formlar
 {
@@ -15,6 +16,37 @@ namespace is_takip_proje.Formlar
         public FrmPersonelIstatistik()
         {
             InitializeComponent();
+        }
+        DbIsTakipEntities db = new DbIsTakipEntities();
+        private void FrmPersonelIstatistik_Load(object sender, EventArgs e)
+        {
+            LblToplamDepartman.Text = db.TblDepartmanlar.Count().ToString();
+            LblToplamFirma.Text = db.TblFirmalar.Count().ToString();
+            LblToplamPersonel.Text = db.TblPersonel.Count().ToString();
+            LblAktifIs.Text = db.TblGorevler.Count(x => x.Durum == "1").ToString();
+            LblPasifIs.Text = db.TblGorevler.Count(x => x.Durum == "0").ToString();
+            LblSonGorev.Text = db.TblGorevler.OrderByDescending(x => x.ID).Select(x => x.Aciklama).FirstOrDefault().ToString();
+            LblSehirSayisi.Text = db.TblFirmalar.Select(x => x.İl).Distinct().Count().ToString();
+            LblSektor.Text = db.TblFirmalar.Select(x => x.Sektor).Distinct().Count().ToString();
+            DateTime bugun = DateTime.Today;
+            LblBugunAcilanGorevler.Text = db.TblGorevler.Count(x => x.Tarih == bugun).ToString();
+
+            var pers = db.TblGorevler
+                .GroupBy(x => x.GorevAlan)
+                .OrderByDescending(y => y.Count())
+                .Select(z => z.Key)
+                .FirstOrDefault();
+
+            var personel = db.TblPersonel.FirstOrDefault(x => x.ID == pers);
+            LblAyinPersoneli.Text = personel != null ? $"{personel.Ad} {personel.Soyad}" : string.Empty;
+            LblAyinDepartmani.Text = db.TblDepartmanlar.Where(x => x.ID == personel.Departman).Select(y => y.Ad).FirstOrDefault().ToString();
+
+            var sonGorevTarihi = db.TblGorevDetaylar
+                .OrderByDescending(x => x.ID)
+                .Select(x => x.Tarih)
+                .FirstOrDefault();
+            LblSonGorevDetay.Text = sonGorevTarihi.HasValue ? sonGorevTarihi.Value.ToString("yyyy-MM-dd") : string.Empty;
+
         }
     }
 }
