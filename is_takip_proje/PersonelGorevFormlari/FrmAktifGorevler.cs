bfd6bcae9bc5c266a197e2c7bd4a60d9b894349a
Metin Yeni <metinyeni33@gmail.com>
2023-09-27 22:10:37 +0300

    Personel Hakkındaki görevler formlşarı eklendi

---
+++ is_takip_proje/PersonelGorevFormlari/FrmAktifGorevler.cs
@@ -0,0 +1,43 @@
+﻿using is_takip_proje.Entity;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace is_takip_proje.PersonelGorevFormlari
+{
+	public partial class FrmAktifGorevler : Form
+	{
+		public FrmAktifGorevler()
+		{
+			InitializeComponent();
+		}
+
+		DbIsTakipEntities db = new DbIsTakipEntities();
+
+		private void FrmAktifGorevler_Load(object sender, EventArgs e)
+		{
+			var degerler = (from x in db.TblGorevler
+							select new
+							{
+								x.ID,
+								x.Aciklama,
+								x.Tarih,
+								x.GorevAlan,
+								x.TblPersonel.Ad,
+								x.Durum
+							}).Where(x => x.GorevAlan == 5 && x.Durum == true).ToList();
+
+			
+			gridControl1.DataSource = degerler;
+			gridView1.Columns["Durum"].Visible = false;
+			gridView1.Columns["GorevAlan"].Visible = false;
+
+		}
+	}
+}
---
+++ is_takip_proje/PersonelGorevFormlari/FrmPersonelFormu.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace is_takip_proje.PersonelGorevFormlari
+{
+	public partial class FrmPersonelFormu : Form
+	{
+		public FrmPersonelFormu()
+		{
+			InitializeComponent();
+		}
+
+		private void FrmPersonelFormu_Load(object sender, EventArgs e)
+		{
+
+		}
+
+		PersonelGorevFormlari.FrmAktifGorevler frm;
+		private void BtnAktifGorevler_ItemClick(object sender, DevExpress.XtraBars.ItemClickEventArgs e)
+		{
+			if (frm == null || frm.IsDisposed)
+			{
+				frm = new PersonelGorevFormlari.FrmAktifGorevler();
+				frm.MdiParent = this;
+				frm.Show();
+			}
+		}
+	}
+}
